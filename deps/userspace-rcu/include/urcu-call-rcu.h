773ffbe0a3c6fda82317245e805a2fb35c54b5cb
mgood7123 <smallville7123@gmail.com>
2024-03-15 03:37:35 +1000

    try to build userspace-rcu

--- deps/userspace-rcu/include/urcu-call-rcu.h
+++
@@ -1,5 +0,0 @@
-// SPDX-FileCopyrightText: 2018 Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
-//
-// SPDX-License-Identifier: LGPL-2.1-or-later
-
-#include <urcu/call-rcu.h>
--- deps/userspace-rcu/include/urcu/urcu_ref.h
+++
@@ -1,6 +0,0 @@
-// SPDX-FileCopyrightText: 2011 Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
-//
-// SPDX-License-Identifier: LGPL-2.1-or-later
-
-#warning "urcu/urcu_ref.h is deprecated. Please include urcu/ref.h instead."
-#include <urcu/ref.h>
