192da219125315fa913a37827b9159d12fc1eedf
mgood7123 <smallville7123@gmail.com>
2024-04-06 11:04:09 +1000

    cache split program - windows

--- try_get_release.sh
+++ try_get_release.sh
@@ -3,10 +3,37 @@
 # response=$? # 0=match, -1=corrupt, -2=missing
 #
 
+set -v
+set -x
+
 FILE=$4
 TAG=$3
 URL=https://github.com/$1/$2/releases/download/$TAG/$FILE
-
-./split/build/split.exe --join https://github.com/mgood7123/$2/releases/download/$TAG/$FILE --out BUILD_DEBUG || mkdir BUILD_DEBUG
-
-exit 0
+R=$(curl --silent -I $URL | grep -E "^HTTP" | awk -F " " '{print $2}')
+RSHA=$(curl --silent -I $URL.sha512 | grep -E "^HTTP" | awk -F " " '{print $2}')
+if [[ ("$R" = "200" || "$R" = "302") && ("$RSHA" == "200" || "$RSHA" == "302") ]]
+    then
+        echo "file '$FILE' exists in releases"
+        curl -L $URL.sha512 -o $FILE.sha512
+        curl -L $URL -o $FILE
+        cat $FILE.sha512
+        if sha512sum -c $FILE.sha512
+            then
+                echo 'extracting build directory...'
+                tar -xf $FILE
+                echo 'extracted build directory'
+                rm $FILE
+                rm $FILE.sha512
+                exit 0
+            else
+                echo 'build directory cache corrupted, rebuilding'
+                mkdir BUILD_DEBUG
+                rm $FILE
+                rm $FILE.sha512
+                exit -1
+        fi
+    else
+        echo 'build directory does not exist in cache'
+        mkdir BUILD_DEBUG
+        exit -2
+fi
